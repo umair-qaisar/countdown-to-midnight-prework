7bd23f832d1f03d1c7aaa8996c4b7a4e18956d66
Umair Qaisar <qaisaru@icloud.com>
2018-09-13 10:58:00 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,15 +1,16 @@
-def countdown(count)
-   while count >0
-     puts "#{count} seconds!"
-     count-=1
-   end
-   "0 left. HAPPY NEW YEAR!"
- end
+def countdown(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    seconds_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
 
- def countdown_with_sleep(count)
-   while count > 0
-     count-=1
-     sleep(1)
-   end
-     puts "0 left. HAPPY NEW YEAR!"
- end
+def countdown_with_sleep(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    sleep(1)
+    seconds_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
